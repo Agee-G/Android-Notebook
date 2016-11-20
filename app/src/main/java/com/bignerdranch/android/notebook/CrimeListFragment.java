eaf397ade07d6904c7c5dae6bd683236c5a44c8c
G <747579608@qq.com>
2016-11-20 11:37:42 +0800

    n3

--- app/src/main/java/com/bignerdranch/android/notebook/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/notebook/CrimeListFragment.java
@@ -3,10 +3,14 @@ package com.bignerdranch.android.notebook;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
@@ -21,8 +25,16 @@ import java.util.List;
  * Created by G__Agee on 2016/11/3.
  */
 public class CrimeListFragment extends Fragment{
+    private static final String SAVED_SUBTITLE_VISIBLE="subtitle";
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdaptere;
+    private boolean mSubtitleVisible;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle saveInstanceState) {
@@ -31,6 +43,9 @@ public class CrimeListFragment extends Fragment{
         mCrimeRecyclerView=(RecyclerView) view
                 .findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        if(saveInstanceState!=null){
+            mSubtitleVisible=saveInstanceState.getBoolean(SAVED_SUBTITLE_VISIBLE);
+        }
         updateUI();
         return view;
     }
@@ -39,6 +54,56 @@ public class CrimeListFragment extends Fragment{
         super.onResume();
         updateUI();
     }
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(SAVED_SUBTITLE_VISIBLE,mSubtitleVisible);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        super.onCreateOptionsMenu(menu,inflater);
+        inflater.inflate(R.menu.fragment_crime_list,menu);
+
+        MenuItem subtitleItem=menu.findItem(R.id.menu_item_show_subtitle);
+        if(mSubtitleVisible) {
+            subtitleItem.setTitle(R.string.hide_subtitle);
+        }else{
+            subtitleItem.setTitle(R.string.show_subtitle);
+        }
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.menu_item_new_crime:
+                Crime crime=new Crime();
+                CrimeLab.get(getActivity()).addCrime(crime);
+                Intent intent=CrimePagerActivity
+                        .newIntent(getActivity(),crime.getId());
+                startActivity(intent);
+                return true;
+            case R.id.menu_item_show_subtitle:
+                mSubtitleVisible=!mSubtitleVisible;
+                getActivity().invalidateOptionsMenu();
+                updateSubtitle();
+                return true;
+            default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+    private void updateSubtitle(){
+        CrimeLab crimeLab=CrimeLab.get(getActivity());
+        int crimeCount=crimeLab.getCrimes().size();
+        String subtitle=getString(R.string.subtitle_format,crimeCount);
+        if(!mSubtitleVisible){
+            subtitle=null;
+        }
+        AppCompatActivity activity=(AppCompatActivity) getActivity();
+        activity.getSupportActionBar().setSubtitle(subtitle);
+    }
+
     private void updateUI(){
         CrimeLab crimeLab=CrimeLab.get(getActivity());
         List<Crime> crimes=crimeLab.getCrimes();
@@ -49,6 +114,7 @@ public class CrimeListFragment extends Fragment{
         }else {
             mAdaptere.notifyDataSetChanged();
         }
+        updateSubtitle();
 
     }
 
